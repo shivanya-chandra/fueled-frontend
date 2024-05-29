e51e9ae22df55463524be96b7094d503bc298017
Yuvraaj Narula <ynarula678@gmail.com>
2024-05-29 17:16:36 +0530

    routes established

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
-
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import App from './pages/App'; 
+import Fyp from './pages/Fyp'; 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="fyp" element={<Fyp />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
-
-
---
+++ src/pages/Fyp.js
@@ -0,0 +1,6 @@
+function Fyp(){
+    return(
+        <h1>Hello world</h1>
+    );
+}
+export default Fyp;
